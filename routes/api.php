88dbcd00643c0b3170fe905455f85e0e6f34134e
Matheus Vieira Santos <matheus.eu.mv@gmail.com>
2022-07-05 10:26:04 -0300

    changed routes

--- routes/api.php
+++ routes/api.php
@@ -17,12 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user/{uid}', [UserController::class, 'getUser'])->name('api.user.get');
 Route::middleware('auth:sanctum')->patch('/user/{uid}', [UserController::class, 'updateUserWithoutIdUFFS'])->name('api.user.update');
-Route::middleware('auth:sanctum')->patch('/iduffs/user/{uid}', [UserController::class, 'updateUserWithIdUFFS'])->name('api.user.update');
+Route::middleware('auth:sanctum')->patch('/user/iduffs/{uid}', [UserController::class, 'updateUserWithIdUFFS'])->name('api.user.update');
 Route::middleware('auth:sanctum')->put('/user/{uid}', [UserController::class, 'changeUserActivity'])->name('api.user.changeUserActivity');
 Route::middleware('auth:sanctum')->post('/reset-password/{uid}', [AuthController::class, 'resetPassword'])->name('api.auth.reset-password');
 
 Route::post('/login', [AuthController::class, 'login'])->name('api.auth.login');
-Route::post('/iduffs/user', [UserController::class, 'createUserWithIdUFFS'])->name('api.user.createWithIdUFFS');
+Route::post('/user/iduffs', [UserController::class, 'createUserWithIdUFFS'])->name('api.user.createWithIdUFFS');
 Route::post('/user', [UserController::class, 'createUserWithoutIdUFFS'])->name('api.user.createWithoutIdUFFS');
 Route::post('/forgot-password/{uid}', [AuthController::class, 'forgotPassword'])->name('api.auth.forgot-password');
 
