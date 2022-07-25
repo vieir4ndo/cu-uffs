0d207c80930d705c1c53cc524eb7e602d0fecc14
Matheus Vieira Santos <matheus.eu.mv@gmail.com>
2022-07-25 12:47:46 -0300

    Create Install.php

---
+++ app/Console/Commands/Install.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Console\Scheduling\Schedule;
+
+class Install extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command to install project';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->call('migrate');
+        $this->call('db:seed');
+        $this->call('config:clear');
+    }
+}
