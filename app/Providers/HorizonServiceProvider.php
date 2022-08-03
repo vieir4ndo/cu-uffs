8d5ef8a79094b9c3f186b55544b5d9bf4eadd836
Matheus Vieira Santos <matheus.eu.mv@gmail.com>
2022-08-03 13:20:20 -0300

    Update HorizonServiceProvider.php

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -34,9 +34,10 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewHorizon', function ($user) {
-            return in_array($user->email, [
+            return true;
+            /*return in_array($user->email, [
                 //
-            ]);
+            ]);*/
         });
     }
 }
