c36ceb33d359b2eb3d0befa817e3a2327a181301
Matheus Vieira Santos <matheus.eu.mv@gmail.com>
2022-08-15 13:04:00 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -59,10 +59,6 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     });
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
-
 Route::get('/reset-password/{uid}/{token}', [AuthController::class, 'index'])->name('web.auth.index');
 //remover esse abaixo
 Route::get('/reset-password', function () {
