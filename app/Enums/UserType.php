1ea278aff79fe5070fca8e76c27788a45f3c156c
Carlos Eduardo Thomas <78752134+Thomas164-cadu@users.noreply.github.com>
2022-08-16 16:28:48 -0300

    config user

--- app/Enums/UserType.php
+++ app/Enums/UserType.php
@@ -7,16 +7,12 @@ enum UserType: int
     case Employee = 1;
     case Student = 2;
     case RUEmployee = 3;
-<<<<<<< Updated upstream
     case ThirdPartyEmployee = 4;
-    case ThirdPartyCashierEmployee = 5;
-=======
-    case ThirdPartyCashierEmployee = 4;
+    //case ThirdPartyCashierEmployee = 4;
     case CSEmployee = 5;
     case CSUser = 6;
     case CSLocator = 7;
     case CSManager = 8;
 
-    //CSEmployee irá ser como o administrador do cadastro de salas
->>>>>>> Stashed changes
+    //CSManager irá ser como o administrador do cadastro de salas
 }
