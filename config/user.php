d90444706460f977ada3170122a4092b3495ffb4
Carlos Eduardo Thomas <78752134+Thomas164-cadu@users.noreply.github.com>
2022-08-16 16:39:55 -0300

    users config
    configurações para manager do CS

--- config/user.php
+++ config/user.php
@@ -3,13 +3,15 @@
 return [
     'users_allowed_login' => [
         \App\Enums\UserType::ThirdPartyCashierEmployee->value,
-        \App\Enums\UserType::RUEmployee->value
+        \App\Enums\UserType::RUEmployee->value,
+        \App\Enums\UserType::CSManager->value
     ],
 
     'users_auth_iduffs' => [
         \App\Enums\UserType::Employee->value,
         \App\Enums\UserType::Student->value,
-        \App\Enums\UserType::RUEmployee->value
+        \App\Enums\UserType::RUEmployee->value,
+        \App\Enums\UserType::CSManager->value
     ],
 
     'users_without_iduffs' => [
